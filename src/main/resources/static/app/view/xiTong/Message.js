44a243b68d25dd09a582a82baabd27633b594eea
zfmzallhy <KenySee@126.com>
2017-09-13 16:46:12 +0800

    提交用户,角色,机构静态页面

---
+++ src/main/resources/static/app/view/xiTong/Message.js
@@ -0,0 +1,37 @@
+Ext.define('Kits.view.xiTong.Message', {
+    extend: 'Ext.grid.Panel',
+    title: '消息列表',
+    columnLines: true,
+    store: Ext.create('Kits.store.Message'),
+    tools: [
+        {
+            type: 'refresh',
+            tooltip: '刷新',
+            callback: function (panel, tool, event) {
+                panel.getStore().load();
+            }
+        }
+    ],
+    columns: [
+        {
+            xtype: 'rownumberer',
+            text: '序号'
+        },
+        {
+            text: '时间',
+            width:200,
+            dataIndex: 'createTime'
+        },
+        {
+            text: '来源',
+            width:300,
+            dataIndex: 'source'
+        },
+        {
+            text: '消息正文',
+            width:500,
+            dataIndex: 'content'
+        }
+    ]
+})
+;
