62a7c4b1dd81a4897170985ecb6ccd4f32a45c8d
zhoubo <zb@zallhy.com>
2017-09-30 15:55:40 +0800

    角色管理

---
+++ src/main/java/com/snjtjj/mapper/UserRoleMapper.java
@@ -0,0 +1,32 @@
+package com.snjtjj.mapper;
+
+import com.snjtjj.entity.UserRole;
+import com.snjtjj.entity.UserRoleExample;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+@Mapper
+public interface UserRoleMapper {
+    int countByExample(UserRoleExample example);
+
+    int deleteByExample(UserRoleExample example);
+
+    int deleteByPrimaryKey(String id);
+
+    int insert(UserRole record);
+
+    int insertSelective(UserRole record);
+
+    List<UserRole> selectByExample(UserRoleExample example);
+
+    UserRole selectByPrimaryKey(String id);
+
+    int updateByExampleSelective(@Param("record") UserRole record, @Param("example") UserRoleExample example);
+
+    int updateByExample(@Param("record") UserRole record, @Param("example") UserRoleExample example);
+
+    int updateByPrimaryKeySelective(UserRole record);
+
+    int updateByPrimaryKey(UserRole record);
+}
